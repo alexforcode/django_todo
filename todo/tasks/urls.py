266a7bae3e7c483f2ae240232b7395c3a95d725c
Alex Code <alex.for.code@gmail.com>
2020-04-04 22:56:09 +0300

    Update tasks

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='list'),
+    path('update_task/<str:pk>/', views.update_task, name='update_task'),
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -16,3 +16,17 @@ def index(request):
 
     context = {'tasks': tasks, 'task_form': task_form}
     return render(request, 'tasks/index.html', context)
+
+
+def update_task(request, pk):
+    task = Task.objects.get(id=pk)
+    task_form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        task_form = TaskForm(request.POST, instance=task)
+        if task_form.is_valid():
+            task_form.save()
+        return redirect('/')
+
+    context = {'task': task, 'task_form': task_form}
+    return render(request, 'tasks/update.html', context)
