d0294aa50868d27d5ef17bea055a0b943e75c672
Alex Code <alex.for.code@gmail.com>
2020-04-04 22:37:22 +0300

    Form to add tasks to list

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+from .forms import TaskForm
 from .models import Task
 
 
 def index(request):
     tasks = Task.objects.all()
+    task_form = TaskForm()
 
-    context = {'tasks': tasks}
+    if request.method == 'POST':
+        task_form = TaskForm(request.POST)
+        if task_form.is_valid():
+            task_form.save()
+        return redirect('/')
+
+    context = {'tasks': tasks, 'task_form': task_form}
     return render(request, 'tasks/index.html', context)
