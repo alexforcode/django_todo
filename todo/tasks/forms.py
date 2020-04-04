cdd7ce6529560b4119f32406cfbaa317a97a4a4f
Alex Code <alex.for.code@gmail.com>
2020-04-05 01:27:33 +0300

    Styling with bootstrap

--- todo/tasks/forms.py
+++ todo/tasks/forms.py
@@ -1,11 +1,20 @@
 from django import forms
-from django.forms import ModelForm
 
 from .models import Task
 
 
 class TaskForm(forms.ModelForm):
     """ Form for Task model """
+    title = forms.CharField(label='Task', widget=forms.TextInput(
+        attrs={'id': 'task-form-field',
+               'class': 'form-control',
+               'type': 'text'}
+    ))
+    complete = forms.BooleanField(label='Complete', required=False, widget=forms.CheckboxInput(
+        attrs={'id': 'task-complete-check',
+               'class': 'form-check-input'}
+    ))
+
     class Meta:
         model = Task
         fields = '__all__'
