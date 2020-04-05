3043185b4942a28085b7e2874ad04d3bb8359e80
Alex Code <alex.for.code@gmail.com>
2020-04-05 18:32:42 +0300

    Add admin settings

--- todo/tasks/admin.py
+++ todo/tasks/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from .models import Task
 
 
-admin.site.register(Task)
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created', 'complete')
+    list_filter = ('created',)
+    ordering = ('title', 'created')
+    search_fields = ('title',)
